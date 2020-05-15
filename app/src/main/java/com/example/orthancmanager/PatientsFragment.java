2d6b229480df0401823b1518bc8765c105bc2e46
id-05 <id-05@mail.ru>
2020-05-16 01:44:10 +0900

    13

--- app/src/main/java/com/example/orthancmanager/PatientsFragment.java
+++ app/src/main/java/com/example/orthancmanager/PatientsFragment.java
@@ -1,5 +1,8 @@
 package com.example.orthancmanager;
 
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +12,134 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.orthancmanager.datastorage.Patient;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class PatientsFragment extends Fragment {
+
+    private SharedPreferences prefs;
+    private JsonParser parserJson = new JsonParser();
+    private SimpleDateFormat format =new SimpleDateFormat("yyyyMMdd");
+    //SharedPreferences.Editor editor;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_patients, container, false);
+        final View fragmentView = inflater.inflate(R.layout.fragment_patients, container, false);
+            prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        return fragmentView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //String data = prefs.getString("SeachResult", "*");
+        //MainActivity.print("data = "+data);
+    }
+
+    @Override
+    public void setMenuVisibility(boolean menuVisible) {
+        super.setMenuVisibility(menuVisible);
+        if (menuVisible) {
+            String data = prefs.getString("SeachResult", "*");
+            MainActivity.print("data = "+data);
+            getPatientsFromJson(data);
+        }
+        else {
+        }
+    }
+
+    public void getPatientsFromJson(String data){
+        HashMap<String, Patient> patientMap=new HashMap<String, Patient>();
+        JsonArray studies=(JsonArray) parserJson.parse(data);
+        Iterator<JsonElement> studiesIterator=studies.iterator();
+
+        while (studiesIterator.hasNext()) {
+            JsonObject studyData=(JsonObject) studiesIterator.next();
+            JsonObject parentPatientDetails=studyData.get("PatientMainDicomTags").getAsJsonObject();
+            String parentPatientID=studyData.get("ParentPatient").getAsString();
+            String studyId=studyData.get("ID").getAsString();
+            JsonObject studyDetails=studyData.get("MainDicomTags").getAsJsonObject();
+
+            String patientBirthDate="N/A";
+            String patientSex="N/A";
+            String patientName="N/A";
+            String patientId="N/A";
+
+            String patientDobString=null;
+            Date patientDob=null;
+            if(parentPatientDetails.has("PatientBirthDate")) {
+                patientDobString=parentPatientDetails.get("PatientBirthDate").getAsString();
+            }
+
+            try {
+                patientDob = format.parse("19000101");
+                patientDob=format.parse(patientDobString);
+            } catch (Exception e) { }
+
+            if(parentPatientDetails.has("PatientSex")) {
+                patientSex=parentPatientDetails.get("PatientSex").getAsString();
+            }
+
+            if(parentPatientDetails.has("PatientName")) {
+                patientName=parentPatientDetails.get("PatientName").getAsString();
+            }
+            if(parentPatientDetails.has("PatientID")) {
+                patientId=parentPatientDetails.get("PatientID").getAsString();
+            }
+
+            String accessionNumber="N/A";
+            if(studyDetails.has("AccessionNumber")) {
+                accessionNumber=studyDetails.get("AccessionNumber").getAsString();
+            }
+
+            String studyInstanceUid=studyDetails.get("StudyInstanceUID").getAsString();
+
+            String studyDate=null;
+            Date studyDateObject=null;
+            if(studyDetails.has("StudyDate")) {
+                studyDate=studyDetails.get("StudyDate").getAsString();
+            }
+
+            try {
+                studyDateObject=format.parse("19000101");
+                studyDateObject=format.parse(studyDate);
+            } catch (Exception e) { }
+
+            String studyDescription="N/A";
+            if(studyDetails.has("StudyDescription")){
+                studyDescription=studyDetails.get("StudyDescription").getAsString();
+            }
+
+            //Study2 studyObj=new Study2(studyDescription, studyDateObject, accessionNumber, studyId, patientName, patientId, patientDob, patientSex, parentPatientID, studyInstanceUid, null);
+
+            if(!patientMap.containsKey(parentPatientID)) {
+                Patient patient=new Patient(patientName,patientId,patientBirthDate,patientSex,parentPatientID);
+                //    patient.addStudy(studyObj);
+                patientMap.put(parentPatientID, patient);
+            }else {
+                //  patientMap.get(parentPatientID).addStudy(studyObj);
+            }
+
+        }
+        ////
+        Iterator<Map.Entry<String, Patient>> iterator = patientMap.entrySet().iterator();
+
+        while (iterator.hasNext())
+        {
+            Map.Entry<String, Patient> pair = iterator.next();
+            String key = pair.getKey();
+            Patient value = pair.getValue();
+            MainActivity.print(value.patientId + " : " + value.name);
+        }
     }
 }
--- app/src/main/java/com/example/orthancmanager/SeachFragment.java
+++ app/src/main/java/com/example/orthancmanager/SeachFragment.java
@@ -2,8 +2,11 @@ package com.example.orthancmanager;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.format.DateUtils;
 import android.util.Base64;
 import android.view.LayoutInflater;
@@ -49,11 +52,19 @@ public class SeachFragment extends Fragment implements ConnectionCallback{
     OrthancServer server = new OrthancServer();
     private JsonParser parserJson = new JsonParser();
     private SimpleDateFormat format =new SimpleDateFormat("yyyyMMdd");
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View fragmentView = inflater.inflate(R.layout.fragment_seach, container, false);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        editor = prefs.edit();
+        editor.putString("SeachResult","");
+        editor.commit();
+
 
         if(getArguments() != null){
             id = getArguments().getInt("serverid");
@@ -136,27 +147,27 @@ public class SeachFragment extends Fragment implements ConnectionCallback{
             query.add("Query", queryDetails);
             MainActivity.print("query = "+query.toString());
             getOrthancData(server,"/tools/find", query.toString());
-            //ServerPanel.TabChange(1);
+
         }
     };
 
     private void getOrthancData(final OrthancServer server, final String tool, final String param) {
-        new AbstractAsyncWorker<String>(this,server,param) {
+        AsyncTask<Void, Void, String> execute = new AbstractAsyncWorker<String>(this, server, param) {
             @SuppressLint("StaticFieldLeak")
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             protected String doAction() throws Exception {
                 String urlParameters = param;
                 String result = null;
-                String auth =new String(server.login + ":" + server.password);
+                String auth = new String(server.login + ":" + server.password);
                 byte[] data1 = auth.getBytes(UTF_8);
                 String base64 = Base64.encodeToString(data1, Base64.NO_WRAP);
                 try {
-                    String fulladdress = "http://"+server.ipaddress+":"+server.port;
-                    URL url = new URL(fulladdress+tool);
+                    String fulladdress = "http://" + server.ipaddress + ":" + server.port;
+                    URL url = new URL(fulladdress + tool);
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                     connection.setDoOutput(true);
-                    connection.setRequestProperty("Authorization", "Basic "+base64);
+                    connection.setRequestProperty("Authorization", "Basic " + base64);
                     connection.setDoOutput(true);
                     connection.setAllowUserInteraction(false);
                     //connection.setRequestProperty("Content-Length",  String.valueOf(fulladdress+urlParameters));
@@ -166,7 +177,7 @@ public class SeachFragment extends Fragment implements ConnectionCallback{
                     connection.connect();
                     OutputStream os = connection.getOutputStream();
                     os.write(urlParameters.getBytes());
-                    int responseCode=connection.getResponseCode();
+                    int responseCode = connection.getResponseCode();
                     if (responseCode == HttpURLConnection.HTTP_OK) {
                         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
                         String line = null;
@@ -174,28 +185,28 @@ public class SeachFragment extends Fragment implements ConnectionCallback{
                         while ((line = bufferedReader.readLine()) != null) {
                             //if(truestring(line))
                             {
-                              //  int i = line.indexOf("}");
+                                //  int i = line.indexOf("}");
                                 //if(i!= -1){
                                 //    if ((i == (line.length()-1)&(i!=0))) {
-                                        //sb.append(line + ",");
-                                        sb.append(line);
+                                //sb.append(line + ",");
+                                sb.append(line);
                                 //    }else
                                 //    {
                                 //        sb.append(line);
                                 //    }
-                               // }else {
-                                 //   sb.append(line);
-                               // }
+                                // }else {
+                                //   sb.append(line);
+                                // }
                             }
                         }
                         bufferedReader.close();
                         result = sb.toString();
-                    }else {
-                        MainActivity.print( "Error server response =  "+responseCode);
+                    } else {
+                        MainActivity.print("Error server response =  " + responseCode);
                     }
                     os.close();
-                }catch (Exception e) {
-                    MainActivity.print("error get thread :"+e.toString());
+                } catch (Exception e) {
+                    MainActivity.print("error get thread :" + e.toString());
                 }
 
                 return result;
@@ -210,89 +221,11 @@ public class SeachFragment extends Fragment implements ConnectionCallback{
 
     @Override
     public void onSuccess(String data, OrthancServer server, String param) {
-        MainActivity.print("data = "+data);
-        HashMap<String, Patient> patientMap=new HashMap<String, Patient>();
-        JsonArray studies=(JsonArray) parserJson.parse(data);
-        Iterator<JsonElement> studiesIterator=studies.iterator();
+        //MainActivity.print("data = "+data);
+        editor.putString("SeachResult",data);
+        editor.commit();
 
-        while (studiesIterator.hasNext()) {
-            JsonObject studyData=(JsonObject) studiesIterator.next();
-            JsonObject parentPatientDetails=studyData.get("PatientMainDicomTags").getAsJsonObject();
-            String parentPatientID=studyData.get("ParentPatient").getAsString();
-            String studyId=studyData.get("ID").getAsString();
-            JsonObject studyDetails=studyData.get("MainDicomTags").getAsJsonObject();
-
-            String patientBirthDate="N/A";
-            String patientSex="N/A";
-            String patientName="N/A";
-            String patientId="N/A";
-
-            String patientDobString=null;
-            Date patientDob=null;
-            if(parentPatientDetails.has("PatientBirthDate")) {
-                patientDobString=parentPatientDetails.get("PatientBirthDate").getAsString();
-            }
-
-            try {
-                patientDob = format.parse("19000101");
-                patientDob=format.parse(patientDobString);
-            } catch (Exception e) { }
-
-            if(parentPatientDetails.has("PatientSex")) {
-                patientSex=parentPatientDetails.get("PatientSex").getAsString();
-            }
-
-            if(parentPatientDetails.has("PatientName")) {
-                patientName=parentPatientDetails.get("PatientName").getAsString();
-            }
-            if(parentPatientDetails.has("PatientID")) {
-                patientId=parentPatientDetails.get("PatientID").getAsString();
-            }
-
-            String accessionNumber="N/A";
-            if(studyDetails.has("AccessionNumber")) {
-                accessionNumber=studyDetails.get("AccessionNumber").getAsString();
-            }
-
-            String studyInstanceUid=studyDetails.get("StudyInstanceUID").getAsString();
-
-            String studyDate=null;
-            Date studyDateObject=null;
-            if(studyDetails.has("StudyDate")) {
-                studyDate=studyDetails.get("StudyDate").getAsString();
-            }
-
-            try {
-                studyDateObject=format.parse("19000101");
-                studyDateObject=format.parse(studyDate);
-            } catch (Exception e) { }
-
-            String studyDescription="N/A";
-            if(studyDetails.has("StudyDescription")){
-                studyDescription=studyDetails.get("StudyDescription").getAsString();
-            }
-
-            //Study2 studyObj=new Study2(studyDescription, studyDateObject, accessionNumber, studyId, patientName, patientId, patientDob, patientSex, parentPatientID, studyInstanceUid, null);
-
-            if(!patientMap.containsKey(parentPatientID)) {
-                Patient patient=new Patient(patientName,patientId,patientBirthDate,patientSex,parentPatientID);
-            //    patient.addStudy(studyObj);
-                patientMap.put(parentPatientID, patient);
-            }else {
-              //  patientMap.get(parentPatientID).addStudy(studyObj);
-            }
-
-        }
-        ////
-        Iterator<Map.Entry<String, Patient>> iterator = patientMap.entrySet().iterator();
-
-        while (iterator.hasNext())
-        {
-            Map.Entry<String, Patient> pair = iterator.next();
-            String key = pair.getKey();
-            Patient value = pair.getValue();
-            MainActivity.print(value.patientId + " : " + value.name);
-        }
+        ServerPanel.TabChange(1);
     }
 
     @Override
